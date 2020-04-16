a81b84e2a73f0082e9f8a2b7703ae2c9c58b802d
lev <juliafrare7@gmail.com>
2020-04-15 21:15:43 -0300

    affinity boundaries now go from 100 to -100, added boundary checks

--- classes/rel.py
+++ classes/rel.py
@@ -7,59 +7,59 @@ class Rel:
        self.affinity = numpy.zeros((size, size))
 
     #affinity system
-    #-10 to -7 = high disaffection
-    #-7 to -4 = medium disaffection
-    #-4 to -2 = mild disaffection
-    #-2 to 2 = neutral
-    #2 to 4 = mild affinity
-    #4 to 7 = medium affinity
-    #7 to 10 = high affinity
+    #-100 to -70 = high disaffection
+    #-70 to -40 = medium disaffection
+    #-40 to -20 = mild disaffection
+    #-20 to 20 = neutral
+    #20 to 40 = mild affinity
+    #40 to 70 = medium affinity
+    #70 to 100 = high affinity
 
     def is_neutral(self, c1, c2):
-        if abs(self.affinity[c1][c2]) < 2:
+        if abs(self.affinity[c1][c2]) < 20:
             return True
         else:
             return False
 
     def mild_affinity(self, c1, c2):
-        if self.affinity[c1][c2] > 2 and self.affinity[c1][c2] < 4:
+        if self.affinity[c1][c2] > 20 and self.affinity[c1][c2] < 40:
             return True
         else:
             return False
 
     def medium_affinity(self, c1, c2):
-        if self.affinity[c1][c2] > 4 and self.affinity[c1][c2] < 7:
+        if self.affinity[c1][c2] > 40 and self.affinity[c1][c2] < 70:
             return True
         else:
             return False
     
     def high_affinity(self, c1, c2):
-        if self.affinity[c1][c2] > 7:
+        if self.affinity[c1][c2] > 70:
             return True
         else:
             return False
     
     def mild_disaff(self, c1, c2):
-        if self.affinity[c1][c2] < -2 and self.affinity[c1][c2] > -4:
+        if self.affinity[c1][c2] < -20 and self.affinity[c1][c2] > -40:
             return True
         else:
             return False
 
     def medium_disaff(self, c1, c2):
-        if self.affinity[c1][c2] < -4 and self.affinity[c1][c2] > -7:
+        if self.affinity[c1][c2] < -40 and self.affinity[c1][c2] > -70:
             return True
         else:
             return False
     
     def high_disaff(self, c1, c2):
-        if self.affinity[c1][c2] < -7:
+        if self.affinity[c1][c2] < -70:
             return True
         else:
             return False
 
     #finds the person with highest affinity with a contestant in the cast
     def find_highest_aff(self, c1):
-        highest = (None, -10)
+        highest = (None, -100)
         for i in range(len(self.affinity[c1])):
             if i == c1:
                 pass
@@ -69,10 +69,46 @@ class Rel:
 
     #finds the person with lowest affinity with a contestant in the cast
     def find_lowest_aff(self, c1):
-        lowest = (None, 10)
+        lowest = (None, 100)
         for i in range(len(self.affinity[c1])):
             if i == c1:
                 pass
             elif self.affinity[c1][i] <= lowest[1]:
                 lowest = (i, self.affinity[c1][i])
         return lowest
+
+    #triggers relationship change between two contestants
+    def rel_change(self, a_index, b_index, a_change, b_change):
+        if self.rc_boundary_check(a_change, a_index, b_index) == True:
+            self.affinity[a_index][b_index] = 100
+        else:
+            self.affinity[a_index][b_index] += a_change
+
+        if self.rc_boundary_check(b_change, b_index, a_index) == True:  
+            self.affinity[b_index][a_index] = 100
+        else:
+            self.affinity[b_index][a_index] += b_change
+
+    #checks if relationship change is inside the boundaries
+    def rc_boundary_check(self, a_change, a_index, b_index):
+        if a_change == 0 and self.affinity[a_index][b_index] > 100:
+            return True
+        elif a_change == 0 and self.affinity[a_index][b_index] < -100:
+            return True
+        elif a_change > 0 and a_change > (100 - self.affinity[a_index][b_index]):
+            return True
+        elif a_change < 0 and a_change < (-100 - self.affinity[a_index][b_index]):
+            return True
+        return False
+
+    #checks if relationship is inside the boundaries (-100 <= aff <= 100) and corrects to 100
+    def boundary_check_correct(self, a_index, b_index):
+        if self.affinity[a_index][b_index] > 100:
+            self.affinity[a_index][b_index] = 100
+        elif self.affinity[a_index][b_index] < -100:
+            self.affinity[a_index][b_index] = -100
+
+        if self.affinity[b_index][a_index] > 100:
+            self.affinity[b_index][a_index] = 100
+        elif self.affinity[b_index][a_index] > -100:
+            self.affinity[b_index][a_index] = -100
